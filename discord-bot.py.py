4562def9b5e8b4724815481b6129f41fdbdf361b
noob002 <92574906+noob002@users.noreply.github.com>
2021-10-20 12:32:54 +0900

    Update discord-bot.py.py

--- discord-bot.py.py
+++ discord-bot.py.py
@@ -210,4 +210,4 @@ async def 명령어(ctx):
 \n검색은 정확하지 않으니 주의 하세요.
 \nURL 에서만 자막이 작동 합니다.""", color = 0x00ff00))
 
-bot.run('ODczNDk1ODUzNjM2MTQxMTE2.YQ5QaA.V1aA7cS4GcM4nKiLUM6O4_grboM')
+bot.run('ODczNDk1ODUzNjM2MTQxMTE2.YQ5QaA.IC1apt6JVHlChD2kAhOUtUN5d_s')
